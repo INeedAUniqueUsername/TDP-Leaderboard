24406479cab8e64ee199896f947f6d23942c70f0
Alex Chen <achen115@ucr.edu>
2020-03-07 15:52:24 -0800

    add CORS and improve Record system

--- TDP-Leaderboard/server.js
+++ TDP-Leaderboard/server.js
@@ -4,9 +4,49 @@ var port = process.env.PORT || 1337;
 port = 8125
 const url = require('url');
 const fs = require('fs');
+const crypto = require('crypto');
 const maps = require('./maps.js');
 const Game = require('./game.js');
-const Leaderboard = require('./Leaderboard.js');
+class Leaderboard {
+  constructor(records) {
+    this.records = [];
+    this.recordsByLevel = {};
+    this.recordsById = {};
+    Object.keys(maps).forEach(level => this.recordsByLevel[level] = []);
+    if (records) {
+      this.addAllRecords(records);
+    }
+  }
+  addSorted(recordSet, record) {
+    if (recordSet.length === 0) {
+      recordSet.push(record);
+      return 0;
+    } else {
+      let times = recordSet.map(r => r.time)
+      let index = binarySearchRight(times, record.time);
+      if (index === times.length) {
+        recordSet.push(record);
+      } else {
+        recordSet.splice(index, 0, record);
+      }
+      return index;
+    }
+  }
+  addAllRecords(records) {
+    records.forEach(record => {
+      record = new Record(record);
+      this.records.push(record);
+      this.recordsById[record.id] = record;
+      this.addSorted(this.recordsByLevel[record.level], record)
+    });
+  }
+  addRecord(record) {
+    this.records.push(record);
+    this.recordsById[record.id] = record;
+    let levelIndex = this.addSorted(this.recordsByLevel[record.level], record);
+    return levelIndex;
+  }
+}
 function binarySearchRight(array, value) {
   let index = Math.floor(array.length / 2);
   let jump = Math.floor(1 + index / 2);
@@ -36,21 +76,21 @@ function binarySearchRight(array, value) {
   return index;
 }
 class Record {
-  constructor(username, replayString, time) {
-    this.username = username;
-    this.level = level;
-    this.replayString = replayString;
-    this.time = time;
+  constructor(from) {
+    this.username = from.username;
+    this.level = from.level;
+    this.replayString = from.replayString;
+    this.time = from.time;
+    this.frames = from.frames;
+    this.id = from.id || crypto.randomBytes(16).toString('hex');
   }
 }
 let leaderboard = new Leaderboard();
 if (fs.existsSync('./records.json')) {
   leaderboard = new Leaderboard(JSON.parse(fs.readFileSync('./records.json')));
-} else {
-  Object.keys(maps).forEach(title => leaderboard.recordsByLevel[title] = []);
 }
 let dataChanged = false;
-let backupInterval = 1000 * 60;
+let backupInterval = 1000 * 60 / 60;
 function backup() {
   if (dataChanged) {
     function name(i) {
@@ -66,8 +106,9 @@ function backup() {
       rename(name(i), name(i + 1));
     }
     rename('./records.json', name(0));
-    fs.writeFileSync('./records.json', JSON.stringify(leaderboard.recordsByLevel));
+    fs.writeFileSync('./records.json', JSON.stringify(leaderboard.records));
   }
+  dataChanged = false;
   setTimeout(backup, backupInterval);
 }
 setTimeout(backup, backupInterval);
@@ -83,10 +124,8 @@ http.createServer(function (req, res) {
   };
   function accept(response) {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     response = JSON.stringify(response);
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.end(response);
     console.log('Response sent: ' + response);
   }
@@ -96,16 +135,17 @@ http.createServer(function (req, res) {
   https://gist.github.com/nilcolor/816580
   if (req.method === 'OPTIONS') {
     console.log('!OPTIONS');
-    var headers = {};
-    // IE8 does not allow domains to be specified, just the *
-    // headers["Access-Control-Allow-Origin"] = req.headers.origin;
-    headers["Access-Control-Allow-Origin"] = "*";
-    headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-    headers["Access-Control-Allow-Credentials"] = false;
-    headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-    headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
-    res.writeHead(200, headers);
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, access-control-allow-origin");
+    //res.setHeader("Access-Control-Allow-Headers", req.headers);
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    res.setHeader("Access-Control-Allow-Credentials", false);
+    res.setHeader("Access-Control-Max-Age", '86400'); // 24 hours
+    res.statusCode = 204;
     res.end();
+
+    console.log(req.headers);
   } else if (u.pathname == '/submit-score' && req.method === 'POST') {
     let level = u.query.level;
     if (!level) {
@@ -133,20 +173,29 @@ http.createServer(function (req, res) {
     }
 
     let g = new Game(map, replayString);
-    let time = 0;
-    while (!g.finish && time < 1000) {
+    let frames = 0;
+    while (!g.finish && frames < 1000) {
       g.stepPlayer();
-      time++;
+      frames++;
     }
     if (!g.finish) {
       reject('Replay does not finish within the time limit');
       return;
     }
-    let index = leaderboard.addRecord(level, new Record(username, replayString, time));
+    let time = g.timer;
+
+    let levelIndex = leaderboard.addRecord(new Record({
+      username: username,
+      level: level,
+      replayString: replayString,
+      time: time,
+      frames: frames
+    }));
     accept(JSON.stringify({
       accepted: true,
-      index: index,
-      time: time
+      levelIndex: levelIndex,
+      time: time,
+      frames: frames
     }));
     dataChanged = true;
   } else if (u.pathname == '/top-ten' && req.method === 'GET') {
